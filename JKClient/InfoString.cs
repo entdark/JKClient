19804d18424d1bd69ea02a04c50629637a9c7305
entdark <entdark@mail.ru>
2021-12-02 01:53:25 +0300

    Optimized InfoString to use EqualityComparer instead of lowering keys for case-insensitive comparison

--- JKClient/InfoString.cs
+++ JKClient/InfoString.cs
@@ -1,27 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace JKClient {
 	internal sealed class InfoString : Dictionary<string, string> {
+		private const char Delimiter = '\\';
 		public new string this[string key] {
-			get => this.ContainsKey(key.ToLower()) ? base[key.ToLower()] : string.Empty;
-			internal set => base[key.ToLower()] = value;
+			get => this.ContainsKey(key) ? base[key] : string.Empty;
+			internal set => base[key] = value;
 		}
-		public InfoString(string infoString) {
+		private InfoString() {}
+		public InfoString(string infoString) : base(new InfoStringComparer()) {
 			if (string.IsNullOrEmpty(infoString)) {
 				return;
 			}
-			int index = infoString.IndexOf('\\');
+			int index = infoString.IndexOf(InfoString.Delimiter);
 			if (index < 0) {
 				return;
 			}
-			if (index == 0) {
-				infoString = infoString.Substring(index+1);
-			}
-			string []keyValuePairs = infoString.Split('\\');
-			int length = keyValuePairs.Length & ~1;
-			for (int i = 0; i < length; i+=2) {
-				this[keyValuePairs[i].ToLower()] = keyValuePairs[i+1];
+			string []keyValuePairs = infoString.Split(InfoString.Delimiter);
+			int i = index != 0 ? 0 : 1;
+			int length = (keyValuePairs.Length - i) & ~1;
+			for (; i < length; i+=2) {
+				this[keyValuePairs[i]] = keyValuePairs[i+1];
 			}
 		}
 		public override string ToString() {
@@ -30,12 +31,20 @@ namespace JKClient {
 			}
 			var builder = new StringBuilder();
 			foreach (var keyValuePair in this) {
-				builder.Append('\\');
+				builder.Append(InfoString.Delimiter);
 				builder.Append(keyValuePair.Key);
-				builder.Append('\\');
+				builder.Append(InfoString.Delimiter);
 				builder.Append(keyValuePair.Value);
 			}
 			return builder.ToString();
+		}
+		private class InfoStringComparer : EqualityComparer<string> {
+			public override bool Equals(string x, string y) {
+				return x.Equals(y, StringComparison.OrdinalIgnoreCase);
+			}
+			public override int GetHashCode(string obj) {
+				return obj.GetHashCode();
+			}
 		}
 	}
 }
