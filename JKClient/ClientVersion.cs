ee19b5f19ec7d32d4d1382a162f4f82207d0655d
entdark <entdark@mail.ru>
2023-09-26 01:13:56 +0300

    ClientVersion does not define values explicitly anymore

--- JKClient/ClientVersion.cs
+++ JKClient/ClientVersion.cs
@@ -1,10 +1,10 @@
 ﻿namespace JKClient {
 	public enum ClientVersion {
-		JA_v1_00 = 1_00,
-		JA_v1_01 = 1_01,
-		JO_v1_02 = 1_02,
-		JO_v1_03 = 1_03,
-		JO_v1_04 = 1_04,
-		Q3_v1_32 = 1_32
+		JA_v1_00,
+		JA_v1_01,
+		JO_v1_02,
+		JO_v1_03,
+		JO_v1_04,
+		Q3_v1_32
 	}
 }
